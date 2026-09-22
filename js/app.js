31514a92e23913d71f1237c9a62001e4d22a0ca3
TT-sensei <rlrieaeoy@gmail.com>
2026-09-23 06:27:42 +0900

    共有リンクの閲覧モードを適用

--- js/app.js
+++ js/app.js
@@ -138,7 +138,8 @@ function bindAppEvents() {
 }
 
 function startApp() {
-  WEEKLY_SHARE.loadShareStateFromURL();
+  const shareMode = WEEKLY_SHARE.loadShareStateFromURL();
+  if (shareMode === "view") document.body.classList.add("share-view");
   WEEKLY_EDITOR.bindEditorEvents();
   WEEKLY_TIMETABLE.bindTimetableEvents();
   WEEKLY_FILE.bindFileEvents();
